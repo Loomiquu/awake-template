6a26dc3c578b439545a70af0c8c58baf5be43f29
MokahMokah <107811831+Loomiquu@users.noreply.github.com>
2023-07-08 15:37:54 -0500

    index

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,5 @@
 <template>
-  <div id="home-page" class="page-wrapper home-page">
-    <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
-      <button
-        v-if="$siteConfig.newsletter.on"
-        class="button is-primary"
-        @click="$eventBus.$emit('modal-triggered', 'newsletter-modal')"
-      >
-        Subscribe To Newsletter
-      </button>
-    </site-hero>
-    <main-section theme="one-column">
-      <template v-slot:default>
-        <!-- All Posts -->
-        <posts-grid />
-      </template>
-      <template v-slot:sidebar>
-        Nothing here
-      </template>
-    </main-section>
-    <news-letter-form-modal />
-  </div>
+  <p>Print Hello World</p>
 </template>
 
 <script>
